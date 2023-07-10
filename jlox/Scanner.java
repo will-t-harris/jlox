421234382b1e835ebc6e34bc1788f6cf43e3b03f
Will Harris <will.t.harris@protonmail.com>
2023-07-10 10:02:13 -0700

    implement scanner and simplest lexemes

---
+++ jlox/Scanner.java
@@ -0,0 +1,117 @@
+package jlox;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static jlox.TokenType.*;
+
+class Scanner {
+	private final String source;
+	private final List<Token> tokens = new ArrayList<>();
+	// start/current index into the string (current token)
+	private int start = 0;
+	private int current = 0;
+	// line tracks current source line
+	private int line = 1;
+
+	Scanner(String source) {
+		this.source = source;
+	}
+
+	List<Token> scanTokens() {
+		while (!isAtEnd()) {
+			start = current;
+			scanToken();
+		}
+
+		tokens.add(new Token(EOF, "", null, line));
+		return tokens;
+	}
+
+	private void scanToken() {
+		char c = advance();
+
+		switch (c) {
+		case '(':
+			addToken(LEFT_PAREN);
+			break;
+		case ')':
+			addToken(RIGHT_PAREN);
+			break;
+		case '{':
+			addToken(LEFT_BRACE);
+			break;
+		case '}':
+			addToken(RIGHT_BRACE);
+			break;
+		case ',':
+			addToken(COMMA);
+			break;
+		case '.':
+			addToken(DOT);
+			break;
+		case '-':
+			addToken(MINUS);
+			break;
+		case '+':
+			addToken(PLUS);
+			break;
+		case ';':
+			addToken(SEMICOLON);
+			break;
+		case '*':
+			addToken(STAR);
+			break;
+
+		// Possible one or two character lexemes
+		case '!':
+			addToken(match('=') ? BANG_EQUAL : BANG);
+			break;
+		case '=':
+			addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+			break;
+		case '<':
+			addToken(match('=') ? LESS_EQUAL : LESS);
+			break;
+		case '>':
+			addToken(match('=') ? GREATER_EQUAL : GREATER);
+			break;
+
+		// Report error if there was no match
+		default:
+			Lox.error(line, "Unexpected character.");
+			break;
+		}
+	}
+
+	private boolean match(char expected) {
+		if (isAtEnd())
+			return false;
+		if (source.charAt(current) != expected)
+			return false;
+
+		// only consume the character if it's what we're looking for
+		current++;
+		return true;
+	}
+
+	private boolean isAtEnd() {
+		return current >= source.length();
+	}
+
+	// consume and return the next character in source file
+	private char advance() {
+		return source.charAt(current++);
+	}
+
+	private void addToken(TokenType type) {
+		addToken(type, null);
+	}
+
+	private void addToken(TokenType type, Object literal) {
+		String text = source.substring(start, current);
+		tokens.add(new Token(type, text, literal, line));
+	}
+}
