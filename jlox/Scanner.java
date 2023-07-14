8363108be9901a67a48f378109d7a6b315a378bb
Will Harris <will.t.harris@protonmail.com>
2023-07-14 08:44:44 -0700

    add logic to scanner for keywords

--- jlox/Scanner.java
+++ jlox/Scanner.java
@@ -10,11 +10,33 @@ import static jlox.TokenType.*;
 class Scanner {
 	private final String source;
 	private final List<Token> tokens = new ArrayList<>();
+	private static final Map<String, TokenType> keywords;
 	// start/current index into the string (current token)
 	private int start = 0;
 	private int current = 0;
 	// line tracks current source line
 	private int line = 1;
+
+	static {
+		keywords = new HashMap<>();
+		keywords.put("and", AND);
+		keywords.put("class", CLASS);
+		keywords.put("else", ELSE);
+		keywords.put("false", FALSE);
+		keywords.put("for", FOR);
+		keywords.put("fun", FUN);
+		keywords.put("if", IF);
+		keywords.put("nil", NIL);
+		keywords.put("or", OR);
+		keywords.put("print", PRINT);
+		keywords.put("return", RETURN);
+		keywords.put("super", SUPER);
+		keywords.put("this", THIS);
+		keywords.put("true", TRUE);
+		keywords.put("var", VAR);
+		keywords.put("while", WHILE);
+
+	}
 
 	Scanner(String source) {
 		this.source = source;
@@ -120,7 +142,14 @@ class Scanner {
 			advance();
 		}
 
-		addToken(IDENTIFIER);
+		String text = source.substring(start, current);
+		TokenType type = keywords.get(text);
+
+		if (type == null) {
+			type = IDENTIFIER;
+		}
+
+		addToken(type);
 	}
 
 	private void number() {
