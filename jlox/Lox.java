358276d43a324528c498a64f672294f8f779d06a
Will Harris <will.t.harris@protonmail.com>
2023-08-01 09:34:08 -0700

    implement Parser class

--- jlox/Lox.java
+++ jlox/Lox.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 public class Lox {
 	static boolean hadError = false;
-	
+
 	public static void main(String[] args) throws IOException {
 		if (args.length > 1) {
 			System.out.println("Usage: jlox [script]");
@@ -22,20 +22,20 @@ public class Lox {
 			runPrompt();
 		}
 	}
-	
+
 	private static void runFile(String path) throws IOException {
 		byte[] bytes = Files.readAllBytes(Paths.get(path));
 		run(new String(bytes, Charset.defaultCharset()));
-		
+
 		// Indicate an error in the exit code
 		if (hadError) System.exit(65);
 	}
-	
+
 	private static void runPrompt() throws IOException {
 		InputStreamReader input = new InputStreamReader(System.in);
 		BufferedReader reader = new BufferedReader(input);
-		
-		for (;;) {
+
+		for (; ; ) {
 			System.out.print("< ");
 			String line = reader.readLine();
 			if (line == null) break;
@@ -43,23 +43,36 @@ public class Lox {
 			hadError = false;
 		}
 	}
-	
+
 	private static void run(String source) {
 		Scanner scanner = new Scanner(source);
 		List<Token> tokens = scanner.scanTokens();
-		
-		for (Token token : tokens) {
-			System.out.println(token);
+		Parser parser = new Parser(tokens);
+		Expr expression = parser.parse();
+
+		// Stop if there was a syntax error
+		if (hadError) {
+			return;
 		}
+
+		System.out.println(new AstPrinter().print(expression));
 	}
-	
+
 	static void error(int line, String message) {
 		report(line, "", message);
 	}
-	
+
 	private static void report(int line, String where, String message) {
 		System.err.println("[line " + line + "] Error" + where + ": " + message);
 		hadError = true;
+	}
+
+	static void error(Token token, String message) {
+		if (token.type == TokenType.EOF) {
+			report(token.line, " at end", message);
+		} else {
+			report(token.line, " at '" + token.lexeme + "'", message);
+		}
 	}
 
 }
