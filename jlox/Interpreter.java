a2695ced3a86cb1419ee88a58941e51301132210
Will Harris <will.t.harris@protonmail.com>
2023-08-09 10:04:52 -0700

    add global variable assignment expressions

--- jlox/Interpreter.java
+++ jlox/Interpreter.java
@@ -136,6 +136,15 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
         return null;
     }
 
+    @Override
+    public Object visitAssignExpr(Expr.Assign expr) {
+        Object value = evaluate(expr.value);
+
+        environment.assign(expr.name, value);
+
+        return value;
+    }
+
     private Object evaluate(Expr expr) {
         return expr.accept(this);
     }
--- jlox/tool/GenerateAst.java
+++ jlox/tool/GenerateAst.java
@@ -14,8 +14,14 @@ public class GenerateAst {
 
         String outputDir = args[0];
 
-        defineAst(outputDir, "Expr", Arrays.asList("Binary: Expr left, Token operator, Expr right",
-                "Grouping:Expr expression", "Literal:Object value", "Variable : Token name", "Unary:Token operator, Expr right"));
+        defineAst(outputDir, "Expr", Arrays.asList(
+                "Assign   : Token name, Expr value",
+                "Binary   : Expr left, Token operator, Expr right",
+                "Grouping : Expr expression",
+                "Literal  : Object value",
+                "Variable : Token name",
+                "Unary    : Token operator, Expr right"
+        ));
 
         defineAst(outputDir, "Stmt", Arrays.asList(
                 "Expression : Expr expression",
