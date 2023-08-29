3b96ec744972ce9911aed37f7c15afa8c2b0f30c
Will Harris <will.t.harris@protonmail.com>
2023-08-29 08:58:11 -0700

    add getAt and assignAt methods to the environment to help with resolving

--- jlox/Environment.java
+++ jlox/Environment.java
@@ -21,6 +21,26 @@ class Environment {
         values.put(name, value);
     }
 
+    // walk the given distance up the chain of parent scopes to find the correct
+    // environment
+    Environment ancestor(int distance) {
+        Environment environment = this;
+
+        for (int i = 0; i < distance; i++) {
+            environment = environment.enclosing;
+        }
+
+        return environment;
+    }
+
+    Object getAt(int distance, String name) {
+        return ancestor(distance).values.get(name);
+    }
+
+    void assignAt(int distance, Token name, Object value) {
+        ancestor(distance).values.put(name.lexeme, value);
+    }
+
     Object get(Token name) {
         if (values.containsKey(name.lexeme)) {
             return values.get(name.lexeme);
